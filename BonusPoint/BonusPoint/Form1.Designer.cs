6e1ed235bf74079c33abeee2772a10a305c5bc72
someone <someone@someplace.com>
2018-03-14 00:16:30 +0600

    some init msg

---
+++ BonusPoint/BonusPoint/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace BonusPoint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(174, 315);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "    ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 75;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1.Location = new System.Drawing.Point(22, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(44, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "!!!!!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.Fuchsia;
+            this.button2.Location = new System.Drawing.Point(245, 149);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(41, 30);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "!!!!!";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button3.Location = new System.Drawing.Point(177, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(43, 31);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "!!!!!";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.ForeColor = System.Drawing.Color.Fuchsia;
+            this.button4.Location = new System.Drawing.Point(296, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(41, 33);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "!!!!!";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 337);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ BonusPoint/BonusPoint/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BonusPoint
+{
+    public partial class Form1 : Form
+    {
+
+        int dx = 15;
+        int dy = 16;
+        List<Button> body;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //body = new List<Button>();
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            /*int cnt = 0; 
+            if (cnt%10==0)
+            { 
+                int x1 = new Random().Next(1, Width - 10);
+                Button btn = new Button();
+                btn.Size = new Size(10, 10);
+                btn.Location = new Point(x1, 0);
+                body.Add(btn);
+            }
+            else
+            {
+                
+                for (int i = 1; i < body.Count(); i++)
+                {
+                    int y = body[i].Location.Y;
+                    body[i].Location = new Point(x1, y + dy);
+                }
+            }
+            cnt++;*/
+
+            int cnt = 0;
+            int ch1 = button1.Location.Y;
+            button1.Location = new Point(button1.Location.X, button1.Location.Y + dy);
+            if(button1.Location.Y == Height-1)
+            {
+                button1.Location = new Point(button1.Location.X, 1);
+            }
+
+            int ch2 = button2.Location.Y;
+            button2.Location = new Point(button2.Location.X, button2.Location.Y + dy);
+            if (button2.Location.Y == Height - 1)
+            {
+                button2.Location = new Point(button2.Location.X, 1);
+            }
+
+            int ch3 = button3.Location.Y;
+            button3.Location = new Point(button3.Location.X, button3.Location.Y + dy);
+            if (button3.Location.Y == Height - 1)
+            {
+                button3.Location = new Point(button3.Location.X, 1);
+            }
+
+            int ch4 = button4.Location.Y;
+            button4.Location = new Point(button4.Location.X, button4.Location.Y + dy);
+            if (button4.Location.Y == Height - 1)
+            {
+                button4.Location = new Point(button4.Location.X, 1);
+            }
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+                int x = label1.Location.X;
+                if (e.KeyCode == Keys.Right)
+                {
+                    x += dx;
+                    label1.Location = new Point(x, label1.Location.Y);
+                    if (x >= Width)
+                    {
+                        label1.Location = new Point(0, label1.Location.Y);
+                    }
+                }
+                if(e.KeyCode == Keys.Left)
+                {
+                    x -= dx;
+                    label1.Location = new Point(x, label1.Location.Y);
+                    if (x <= 0)
+                    {
+                        label1.Location = new Point(Width-1, label1.Location.Y);
+                    }
+                }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
