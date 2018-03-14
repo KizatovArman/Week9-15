e66125dd403bde00f995af166101076d21cb8734
someone <someone@someplace.com>
2018-03-14 12:50:25 +0600

    BonusGame

--- BonusPoint/BonusPoint/Form1.cs
+++ BonusPoint/BonusPoint/Form1.cs
@@ -17,24 +17,18 @@ namespace BonusPoint
         int dy = 16;
         int cnt = 0;
         int k = 0;
-        bool canplay = true;
-       
         List<Button> body = new List<Button>(); 
         public Form1()
         {
             InitializeComponent();
         }
-
-       
-
         private void timer1_Tick(object sender, EventArgs e)
         {
-            
             if (cnt%5==0)
             { 
                 int x1 = new Random().Next(1, Width - 60);
                 Button btn = new Button();
-                btn.Size = new Size(15, 15);
+                btn.Size = new Size(24, 24);
                 btn.Location = new Point(x1, 0);
                 btn.BackColor = Color.White;
                 btn.Text = "!!!!";
@@ -46,27 +40,19 @@ namespace BonusPoint
                 }
             }
             else
-            {
-                
+            {        
                 for (int i = 1; i < body.Count(); i++)
                 {
                     int y = body[i].Location.Y;
                     body[i].Location = new Point(body[i].Location.X, y + dy);
-                    if (body[i].Location.Y == Height)
-                        k++;
+                   
                 }
             }
             cnt++;
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-            
-        }
-
+        private void label1_Click(object sender, EventArgs e){}
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            
                 int x = label1.Location.X;
                 if (e.KeyCode == Keys.Right)
                 {
@@ -88,13 +74,10 @@ namespace BonusPoint
                 }
             
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
-           
             this.BackColor = Color.Black;
         }
-
         private void label2_Click(object sender, EventArgs e)
         {
             label2.Text = k.ToString();
